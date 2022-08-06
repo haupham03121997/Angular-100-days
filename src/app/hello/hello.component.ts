25ac02cef8597ef9bec5b533097db03b203c838c
Hau-dev <xuanhau031297@gmail.com>
2022-08-06 11:39:30 +0700

    day 3 : data binding

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -2,10 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-hello",
-  template: `<span>Hello component works</span>`,
+  template: `<span>Hello {{ user.name}}</span>`, // inline template
   styles: [``]
 })
 
 export class HelloComponent {
-
+  user = {
+    name: "John Doe",
+    age: 30
+  }
 }
